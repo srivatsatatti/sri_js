604ca4ab4e50bdf92f34b096f0a2199ac4a487af
srivatsatatti <49546750+srivatsatatti@users.noreply.github.com>
2024-09-10 12:30:10 +0530

    Update cinde.js

--- cinde.js
+++ cinde.js
@@ -7,7 +7,7 @@ var lat_isMin = false;
 var lat_isFrameVisible = false;
 var objElement = window.top.document;
 function LATIconShow() {
-    console.log("=========== Function called ==============");
+    console.log("=========== Function called 01 ==============");
     // Search for the DOM element for the utility menu container
     ['macroponent-f51912f4c700201072b211d4d8c26010',
         'shadowRoot',
